fae74807aa3780fa411b3225d8c4ca0ca08861d4
duykhang28 <duykhang28@gmail.com>
2024-04-29 01:47:22 +1000

    final all

--- resources/views/home/sub_category.blade.php
+++ resources/views/home/sub_category.blade.php
@@ -34,10 +34,18 @@
                     <div class="col-sm-6 col-md-4 col-lg-4">
                         <div class="box product_data">
                             <div class="option_container">
-                                <div class="options">
-                                    <input type="submit" value="Add To Cart" class="btn btn-danger add-to-cart-btn">
-                                    <input type="hidden" class="product_id" value="{{ $products->product_id }}">
-                                </div>
+                                @if ($products->in_stock > 0)
+                                    <div class="options">
+                                        <input type="submit" value="Add To Cart"
+                                            class="btn btn-danger add-to-cart-btn">
+                                        <input type="hidden" class="product_id" value="{{ $products->product_id }}">
+                                    </div>
+                                @else
+                                    <div class="options">
+                                        <input type="submit" value="Out of Stock"
+                                            class="btn btn-secondary add-to-cart-btn" disabled>
+                                    </div>
+                                @endif
                             </div>
                             <div class="img-box">
                                 <img src="{{ $products->image }}" alt="">
